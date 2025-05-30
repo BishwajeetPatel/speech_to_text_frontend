0f462f8cb9f54f2a885db621dd3143f7d5271d2e
BishwajeetPatel <153221241+BishwajeetPatel@users.noreply.github.com>
2025-05-30 07:41:45 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5173/api';
+const API_URL = import.meta.env.VITE_API_URL || 'https://speech-to-text-backend-fheu.onrender.com';
 
 const api = axios.create({
   baseURL: API_URL,
